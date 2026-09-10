a1b9e8131423f0384a248f7f76cd4afd2fd2e0c9
ADROY RENIER CHRISTON NAZARETH <adroynazareth@gmail.com>
2026-09-10 19:15:07 +0530

    Add URL routing

---
+++ config/urls.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import include,path
+from blog.views import register
+urlpatterns=[path('admin/',admin.site.urls),path('accounts/login/',auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),path('accounts/logout/',auth_views.LogoutView.as_view(),name='logout'),path('accounts/register/',register,name='register'),path('',include('blog.urls'))]
